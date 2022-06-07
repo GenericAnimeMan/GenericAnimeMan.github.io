dbdc50e70491e308d22c4c13d7057a7838ee8563
GenericAnimeMan <samando7002@gmail.com>
2022-06-07 17:25:32 +1000

    base

---
+++ script.js
@@ -0,0 +1,12 @@
+$("document").ready( function() {
+    header = `
+        <h1>test header</h1>
+    `
+    $("#header").html(header);
+
+    footer = `
+        <p>don't steal this please i worked really hard on it (not really).</p>
+    `
+    $("#footer").html(footer);
+
+})
